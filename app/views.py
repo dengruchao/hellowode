f6a08b3923ee1376b22c0b172182db78ca99d0c6
dengruchao <517374682@qq.com>
2017-03-02 17:04:28 +0800

    Add wechat_auth and wsgi files

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import request, make_response
+import hashlib
+
+@app.route('/', methods = ['GET', 'POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        token = 'hellowode'
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if hashlib.sha1(s).hexdigest() == signature:
+            return make_response(echostr)
---
+++ index.wsgi
@@ -0,0 +1,9 @@
+import sae
+
+# site-packages
+# sae.add_vendor_dir('vendor')
+
+from app import app
+
+app.debug = True
+application = sae.create_wsgi_app(app)
