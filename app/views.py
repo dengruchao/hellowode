5895b68d42149fe70057de178a807530feaabbe6
dengruchao <517374682@qq.com>
2017-03-02 18:36:12 +0800

    sad

--- app/views.py
+++ app/views.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
+import time
 from app import app
 from flask import request, make_response
 import hashlib
+import xml.etree.ElementTree as ET
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/',methods=['GET','POST'])
 def wechat_auth():
     if request.method == 'GET':
-        token = 'hellowode'
-        query = request.args
-        signature = query.get('signature', '')
-        timestamp = query.get('timestamp', '')
-        nonce = query.get('nonce', '')
-        echostr = query.get('echostr', '')
-        s = [timestamp, nonce, token]
+        token='liusicong'
+        data = request.args
+        signature = data.get('signature','')
+        timestamp = data.get('timestamp','')
+        nonce = data.get('nonce','')
+        echostr = data.get('echostr','')
+        s = [timestamp,nonce,token]
         s.sort()
-        sha1 = hashlib.sha1()
-        map(sha1.update, s)
-        hashcode = sha1.hexdigest()
-        if hashcode == signature:
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
+    else:
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        tou = xml_rec.find('ToUserName').text
+        fromu = xml_rec.find('FromUserName').text
+        content = xml_rec.find('Content').text
+        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(xml_rep % (fromu,tou,str(int(time.time())), content))
+        response.content_type='application/xml'
+        return response
 
