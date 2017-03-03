1b0a1ff6e9d83f77cc0dc66244c6dac595ef6d72
dengruchao <517374682@qq.com>
2017-03-03 18:45:18 +0800

    update

--- app/reply.py
+++ app/reply.py
@@ -40,9 +40,29 @@ class Reply:
 		response.content_type = 'application/xml'
 		return response
 
+	def musicMsg(self):
+		link = 'http://m10.music.126.net/20170303190437/7577ba21915fffd1d3b2bdc098d0950b/ymusic/47fe/b7b0/059b/1527f518027a60ede32fd42f8f1762ab.mp3'
+		xml_resp = '<xml>\
+					<ToUserName><![CDATA[%s]]></ToUserName>\
+					<FromUserName><![CDATA[%s]]></FromUserName>\
+					<CreateTime>%s</CreateTime>\
+					<MsgType><![CDATA[music]]></MsgType>\
+					<Music>\
+					<Title><![CDATA[%s]]></Title>\
+					<Description><![CDATA[%s]]></Description>\
+					<MusicUrl><![CDATA[%s]]></MusicUrl>\
+					</Music>\
+					</xml>'
+		response = make_response(xml_resp % (self.fromUserName, self.toUserName, str(int(time.time())), u'彩虹', 'nothing', link))
+		response.content_type = 'application/xml'
+		return response
+
 	def dispatch(self, msgType, content):
 		if msgType == 'text':
-			return self.textMsg(content)
+			if content == u'文本':
+				return self.textMsg(content)
+			elif content == u'音乐':
+				return self.musicMsg()
 		elif msgType == 'image':
 			return self.imageMsg(content)
 		else:
--- app/wechatInterface.py
+++ app/wechatInterface.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import time
 import pylibmc as memcache
 
 class WechatInterface:
