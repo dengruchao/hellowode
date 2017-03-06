925f0f2ec9d025e16617785e142e9de7bea348ff
dengruchao <517374682@qq.com>
2017-03-06 19:20:54 +0800

    update

--- app/meizitu.py
+++ app/meizitu.py
@@ -11,7 +11,7 @@ class Meizitu:
 		resp = requests.get(self.url)
 		resp.encoding = 'gb2312'
 		page = resp.text
-		pattern = 'href="(.*?)".*?target="_blank".*?title="%s"' % tag
+		pattern = '<a href="(.*?)".*?target="_blank".*?title="%s"' % tag
 		link = re.search(pattern, page, re.S)
 		print link.group(1)
 		resp = requests.get(link.group(1))
