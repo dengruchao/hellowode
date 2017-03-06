e8d220969b84fa98b4fcc489100a573c3497231e
dengruchao <517374682@qq.com>
2017-03-06 19:31:12 +0800

    update

--- app/meizitu.py
+++ app/meizitu.py
@@ -12,16 +12,17 @@ class Meizitu:
 		resp.encoding = 'gb2312'
 		page = resp.text
 		html = etree.HTML(page)
-		tag_url = html.xpath('//*[@id="subcontent clearfix"]/div[2]/span/a[%d]/@href' % tag_index)
+		path = '//*[@id="subcontent clearfix"]/div[2]/span/a[%d]/@href' % (tag_index+1)
+		tag_url = html.xpath(path)[0]
 		print tag_url
 		resp = requests.get(tag_url)
 		page = resp.content
 		html = etree.HTML(page)
 		articals = []
 		for i in range(10):
-			link = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/@href' % i)[0]
-			link_img = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/img')[0]
-			name = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/img/@alt' % i)[0]
+			link = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/@href' % i+1)[0]
+			link_img = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/img' % i+1)[0]
+			name = html.xpath('//*[@id="maincontent"]/div[1]/ul/li[%s]/div/div/a/img/@alt' % i+1)[0]
 			articals.append([name, name, link_img, link])
 		print articals
 		return reply.imgTextMsg(articals)
