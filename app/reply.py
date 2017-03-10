eb9b454c820ce07c2a4b19e56cff2dd97b282cf2
dengruchao <517374682@qq.com>
2017-03-10 13:24:32 +0800

    update

--- app/reply.py
+++ app/reply.py
@@ -134,6 +134,7 @@ class Reply:
         self.toUserName = xml_recv.find('ToUserName').text
         self.fromUserName = xml_recv.find('FromUserName').text
         msgType = xml_recv.find('MsgType').text
+        print msgType
         if msgType == 'text':
             text = xml_recv.find('Content').text
             if text == u'文本':
@@ -159,7 +160,6 @@ class Reply:
             return self.imageMsg(media_id)
         elif msgType == 'event':
             event = xml_recv.find('Event').text
-            return self.textMsg(event)
             if event == 'subscribe':
                 return self.subscribe()
             elif event == 'LOCATION':
