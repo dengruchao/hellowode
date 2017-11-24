debda41cd89a2090512d204c2ea0d272c3c4d9da
rdeng <517374682@qq.com>
2017-11-24 14:15:53 +0800

    Change talentapt

--- app/talentapt.py
+++ app/talentapt.py
@@ -7,11 +7,18 @@ class TalentApt:
         self.session = requests.Session()
 
     def login(self):
-        url = 'http://www.zj-talentapt.com/Login.aspx?flag=0&userName=13166297263&passWord=ZHJjMzA2NDMz&md5=422fe37bc380cb48118133a7d17ae058'
-        resp = self.session.get(url)
+        #url = 'http://www.zj-talentapt.com/Login.aspx?flag=0&userName=13166297263&passWord=ZHJjMzA2NDMz&md5=422fe37bc380cb48118133a7d17ae058'
+        #resp = self.session.get(url)
+        url = 'http://rcgy.zjhui.net/ashx/ApiService.ashx'
+        payload = {
+                'data': {"systemId":"ht_02","mobilePhone":"13166297263","accountType":"1","password":"422fe37bc380cb48118133a7d17ae058"},
+                'url': '/users/login'
+                }
+        resp = self.session.get(url, params=payload)
 
     def getWaitingRecord(self):
-        url = 'http://www.zj-talentapt.com/System/WaitingRecord.aspx'
+        #url = 'http://www.zj-talentapt.com/System/WaitingRecord.aspx'
+        url = 'http://rcgy.zjhui.net/System/WaitingRecord.aspx'
         resp = self.session.get(url)
         html = etree.HTML(resp.text)
         num = html.xpath('//*[@id="ctl00_ctl00_ctl00_main_main_main_rptApplyRecord_ctl00_labPageRank"]/text()')[0]
